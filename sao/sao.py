f8ee486c03e741eafdb6bf85122f3b55f16f67cc
silica <184403842+silica-pina@users.noreply.github.com>
2024-11-21 01:21:12 +0900

    ↓キーを押すことで乱数を取得するようにした

--- sao/sao.py
+++ sao/sao.py
@@ -1,35 +1,46 @@
 import time
 import random as rd
-from multiprocessing import Process
+from multiprocessing import Process, Value
 import tkinter as tk
+import time
 
 li = list(range(65260))
 flg = 0
 
 #ずっと動かすメイン抽選
-def roulette(self):
+def roulette(n):
     r = rd.randint(0,65259)
-    for i in li[r:]:
-        continue
-    for i in li[:r]:
-        continue
+    while True:
+        for i in li[r:]:
+            n.value = i
+        for i in li[:r]:
+            n.value = i
 
-def key_event(e):
-    if e.keysym == "Down":
-        print("↓キーを押すと乱数取得する操作を書く")
 
 #tkinterで状態や乱数、出玉などを表示する
-def output(self):
+def output(n):#ここのｎが変わらんからこの方法では無理か？
+    def key_event(e,n):
+        if e.keysym == "Down":
+            print(n.value)#乱数表示
+
+    def see_n():
+        label["text"] = n.value
+        root.after(1,see_n)
+
     root = tk.Tk()
     root.title("test")
     root.geometry("500x500")
-    root.bind("<KeyPress>",key_event)
+    root.bind("<KeyPress>",lambda e:key_event(e,n))
+    label = tk.Label(font=("Ubunt Mono",160))
+    label.pack()
+    see_n()
     root.mainloop()
 
 #プロセスを動かす
 if __name__=="__main__":
-    p1 = Process(target=roulette, args=(1,))
-    p2 = Process(target=output, args=(2,))
+    n = Value("i",0) #iはintを表す
+    p1 = Process(target=roulette, args=(n,))
+    p2 = Process(target=output, args=(n,))
     p1.start()
     p2.start()
     p1.join()
