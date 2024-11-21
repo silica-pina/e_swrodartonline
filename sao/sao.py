78ab487b5b2c9b16aef1b5cb2502693dee21491f
silica <184403842+silica-pina@users.noreply.github.com>
2024-11-22 00:14:27 +0900

    抽選結果を表示するように変更

--- sao/sao.py
+++ sao/sao.py
@@ -19,13 +19,16 @@ def roulette(n):
 
 #tkinterで状態や乱数、出玉などを表示する
 random = 0
+cnt = 0
 def output(n):#ここのｎが変わらんからこの方法では無理か？
     global random
     def key_event(e,n):
-        global random
+        global random,cnt
         if e.keysym == "Down":
             random = n.value
             print(random)#乱数表示
+            cnt += 1
+            label4["text"] = cnt
 
     def see_random():
         global random
@@ -36,16 +39,31 @@ def output(n):#ここのｎが変わらんからこの方法では無理か？
         label2["text"] = n.value
         root.after(1,see_n)
 
+    def judge():
+        global random,cnt
+        if random % 204 == 0 and random % 2 == 0:
+            label3["text"] = "確変"
+        elif random % 204 == 0 and random % 2 == 1:
+            label3["text"] = "時短"
+        else:
+            label3["text"] = "ハズレ"
+        root.after(1,judge)
+
     root = tk.Tk()
     root.title("test")
-    root.geometry("500x500")
+    root.geometry("500x700")
     root.bind("<KeyPress>",lambda e:key_event(e,n))
-    label1 = tk.Label(font=("Ubunt Mono",130))
-    label2 = tk.Label(font=("Ubunt Mono",130))
+    label1 = tk.Label(font=("Ubunt Mono",110))
+    label2 = tk.Label(font=("Ubunt Mono",110))
+    label3 = tk.Label(font=("Ubunt Mono",110))
+    label4 = tk.Label(font=("Ubunt Mono",80))
     label1.pack()
     label2.pack()
+    label3.pack()
+    label4.pack()
     see_random()
     see_n()
+    judge()
     root.mainloop()
 
 #プロセスを動かす
